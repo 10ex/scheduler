51f7c2ca7b9c6d0f31ad8728506a2f07370d1598
Dakota Murphy <dakota.murphy@shiplux.com>
2019-01-18 19:24:37 -0500

    add sortbyComplexity  method and re-factor complexity function

--- src/server/lib/employee/complexity.js
+++ src/server/lib/employee/complexity.js
@@ -1,24 +1,21 @@
+import R from 'ramda'
+
 /**
- * Create function that given an employee availibility list, calculate the the employees complexity score
- * 
+ * Create function that given an employee availibility list,
+ *  calculate the the employees complexity score
  * [{day: Sunday, hours: [0,1,1, 1, 1, 1, 1, 0, 1] }, .... ,{day: Saturday}]
- * 
+ *
  *  algoithm is sum of the hours availible in an availibility list
  */
+export const complexity = R.compose(
+  R.reduce(R.add, 0),
+  R.flatten,
+  R.map(R.prop('hours')),
+)
 
- const complexity = (availiability) => {
-    let sum = 0
-    for (let i = 0; i < availiability.length; i++) {
 
-        
-        let hours = availiability[i].hours  
-
-        for (let j = 0; j < hours.length; i++) {
-
-           sum = hours[j] + sum
-
-        }
-
-    }
-    return sum 
- }
+// list of employyes   return a sorted list by complexity
+// [employee { id: string, avialibility: [...]} ]
+export const sortByComplexity = R.sort(
+  (a, b) => complexity(b.avialibility) - complexity(a.avialibility),
+)
