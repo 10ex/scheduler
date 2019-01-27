cebe72d5a0e2b787d1d6feebe633d566674da3eb
Dakota Murphy <dakota.murphy@shiplux.com>
2019-01-27 10:58:03 -0500

    add model schema to is availible function is comments

--- src/server/lib/scheduler/shifts.js
+++ src/server/lib/scheduler/shifts.js
@@ -1,3 +1,5 @@
+// shift === { day: 'Sunday', clockIn: 3, clockOut: 11 }
+// employee === { id: string, avialibility:[{day: Sunday, hours: [0,1,1, 1, 1, 1, 1, 0, 1] }, .... ,{day: Saturday}] }
 const isAvailible = (shift, employee) => {
 
 }
