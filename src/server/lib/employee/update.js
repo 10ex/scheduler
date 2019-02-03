74178f2de7ff3f2c465852c0553145f42fb811c9
Noah Arjune <narjune131@gmail.com>
2019-02-03 13:26:52 -0500

    no message

--- src/server/lib/employee/update.js
+++ src/server/lib/employee/update.js
@@ -1,15 +1,18 @@
 // off employees avaliability and add to totalHours, scheduledshifts feild on employee
 // employee === { id: string, avialiability:[{day: Sunday, hours: [0,1,1, 1, 1, 1, 1, 0, 1] }, .... ,{day: Saturday}] }
+// shift === { day: 'Sunday', clockIn: 3, clockOut: 11 }
+
 const applyShift = (shift, employee) => {
-  let totalHours 
- const newAvaliability = employee.avaliability.map()
+  let totalHours
+  const shiftTime = (current, index) => index >= shift.clockIn && index < shift.clockOut ? 0 && totalHours + 1 : current
+
+  const updatedEmployeeList = employee.avaliability.map(day => ({ ...day, hours: day.day === shift.day ? day.hours.map(shiftTime) : day.hours }))
+  return { ...employee, avaliability: updatedEmployeeList }
 }
 
 
 const getUpdatedEmployeeList = (employees, scheduledDay) => {
-  for (let i = 0; i < scheduledDay.length; i++) {
-    for (let j = 0; j < employees; j++) {
-      applyShift(shift.shift, employees[i])
-    }
+  for (let i = 0; i < employees.length; i++) {
+    applyShift(employees[i])
   }
 }
